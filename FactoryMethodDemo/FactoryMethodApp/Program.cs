8ce33874ab8273a18a19d5d5a321f2a7ac700c78
rukmandenysp764-ai <rukman.denys.p764@kfccte-nau.ukr.education>
2025-12-14 14:14:18 +0200

    Add files via upload

---
+++ FactoryMethodDemo/FactoryMethodApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using FactoryMethodClass; // Это станет работать после добавления ссылки
+
+namespace FactoryMethodApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Master[] masters = new Master[]
+            {
+                new ClassicMaster(),
+                new BaroqueMaster(),
+                new ModernMaster()
+            };
+
+            foreach (var master in masters)
+            {
+                Facade facade = master.CreateFacade();
+                facade.Display();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FactoryMethodDemo/FactoryMethodClass/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodClass
+{
+    public abstract class Facade
+    {
+        public abstract void Display();
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class ClassicFacade : Facade
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Classic Facade");
+        }
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class BaroqueFacade : Facade
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Baroque Facade");
+        }
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class ModernFacade : Facade
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Modern Facade");
+        }
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public abstract class Master
+    {
+        public abstract Facade CreateFacade();
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class ClassicMaster : Master
+    {
+        public override Facade CreateFacade()
+        {
+            return new ClassicFacade();
+        }
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class BaroqueMaster : Master
+    {
+        public override Facade CreateFacade()
+        {
+            return new BaroqueFacade();
+        }
+    }
+}
+
+namespace FactoryMethodClass
+{
+    public class ModernMaster : Master
+    {
+        public override Facade CreateFacade()
+        {
+            return new ModernFacade();
+        }
+    }
+}
